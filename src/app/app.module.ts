34d30f17d11f881608edd20a986f359e02600c35
Waylon Carrier Nunes <wayloncn@gmail.com>
2020-07-09 12:24:42 -0400

    reset css and html to remove material design and bootstrap stuff. Also removed the imports in ap_module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,6 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatListModule } from "@angular/material/list";
-import { MatButtonModule } from "@angular/material/button";
-import { MatIconModule } from "@angular/material/icon";
-import { MatSelectModule } from "@angular/material/select";
-import { MatCardModule } from "@angular/material/card";
-import { MatInputModule } from "@angular/material/input";
-import { MatGridListModule } from "@angular/material/grid-list";
-import { MatDividerModule } from "@angular/material/divider";
-import { MatSnackBarModule } from "@angular/material/snack-bar";
-
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
@@ -38,16 +27,6 @@ import { BasicsListComponent } from "./basics-list/basics-list.component";
     FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatListModule,
-    MatButtonModule,
-    MatIconModule,
-    MatSelectModule,
-    MatCardModule,
-    MatInputModule,
-    MatGridListModule,
-    MatDividerModule,
-    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
